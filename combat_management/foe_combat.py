d4e3b398c9d843720fcb74b958b78f43de957f12
Derek Woo <me@derekw.co>
2023-04-05 19:31:39 -0700

    Implement functions in foe_combat.py

---
+++ combat_management/foe_combat.py
@@ -0,0 +1,79 @@
+"""
+Derek Woo
+A01351415
+"""
+
+
+import random
+import itertools
+from time import sleep
+from character_management.manage_character import is_alive
+from character_management import MIN_HEALTH
+from combat_management import TIME_BETWEEN_COMBAT
+
+
+def calculate_damage(fighter: dict) -> int:
+    """
+    Calculate the amount of damage dealt in one combat turn.
+
+    :param fighter: a dictionary representing the character's status
+    :precondition: fighter must be a dictionary
+    :precondition: fighter must contain a "Strength" key associated with an integer greater than or equal to zero
+    :precondition: fighter must contain a "Luck" key associated with an integer greater than or equal to zero
+    :postcondition: calculates the amount of damage dealt in one turn as a function of fighter's strength and luck
+    :return: the amount of damage dealt in one combat turn as an integer
+    """
+    raw_damage = int(fighter["Strength"] * 0.8 + random.randint(0, fighter["Luck"]) * 0.6)
+
+    return max(0, raw_damage)
+
+
+def fight_foe(character: dict, foe: dict) -> bool:
+    """
+    Initiate and complete a fight with a given foe.
+
+    :param character: a dictionary representing the character's current status
+    :param foe: a dictionary representing the foe's current status
+    :precondition: character must be a dictionary
+    :precondition: character must contain a "Current HP" key associated with an integer greater than or equal to zero
+    :precondition: character must contain a "Strength" key associated with an integer greater than or equal to zero
+    :precondition: character must contain a "Luck" key associated with an integer greater than or equal to zero
+    :precondition: foe must be a dictionary
+    :precondition: foe dictionary must contain a "Current HP" key associated with an integer
+    :precondition: foe must contain a "Strength" key associated with an integer greater than or equal to zero
+    :precondition: foe must contain a "Luck" key associated with an integer greater than or equal to zero
+    :postcondition: alternates combat rounds between character and foe, reducing damage as calculated until either dies
+    :return: whether the character is still alive as a boolean
+    """
+    belligerents = (character, foe)
+    switch_turns = itertools.cycle((0, 1))
+
+    while character["Current HP"] > MIN_HEALTH and foe["Current HP"] > MIN_HEALTH:
+        current_turn = next(switch_turns)
+
+        current_fighter = belligerents[current_turn]
+        opposing_fighter = belligerents[(current_turn + 1) % 2]
+
+        damage = calculate_damage(current_fighter)
+        opposing_fighter["Current HP"] -= damage
+
+        if not current_turn:
+            print(f"You inflicted {damage} damage to {opposing_fighter['Name']}.\n")
+
+        else:
+            print(f"{current_fighter['Name']} inflicted {damage} damage to {opposing_fighter['Name']}.")
+            print(f"You have {opposing_fighter['Current HP']}HP left\n")
+
+        sleep(TIME_BETWEEN_COMBAT)
+
+    return is_alive(character=character, alive_message="You win the fight!\n")
+
+
+def main():
+    """
+    Drive the program.
+    """
+
+
+if __name__ == "__main__":
+    main()
