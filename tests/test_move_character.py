30715f49ee7bbc55d9693c4ed8e60b19aefc3e04
Derek Woo <me@derekw.co>
2023-04-05 19:36:09 -0700

    Add unit tests to test_move_character.py

---
+++ tests/test_move_character.py
@@ -0,0 +1,38 @@
+"""
+Derek Woo
+A01351415
+"""
+
+
+from unittest import TestCase
+from character_management.character_movement import move_character
+
+
+class TestMoveCharacter(TestCase):
+    def test_move_character_up(self):
+        test_character = {"X-coordinate": 1, "Y-coordinate": 1}
+        move_character(character=test_character, direction="Up")
+        expected = (1, 0)
+        actual = (test_character["X-coordinate"], test_character["Y-coordinate"])
+        self.assertEqual(expected, actual)
+
+    def test_move_character_right(self):
+        test_character = {"X-coordinate": 1, "Y-coordinate": 1}
+        move_character(character=test_character, direction="Right")
+        expected = (2, 1)
+        actual = (test_character["X-coordinate"], test_character["Y-coordinate"])
+        self.assertEqual(expected, actual)
+
+    def test_move_character_down(self):
+        test_character = {"X-coordinate": 1, "Y-coordinate": 1}
+        move_character(character=test_character, direction="Down")
+        expected = (1, 2)
+        actual = (test_character["X-coordinate"], test_character["Y-coordinate"])
+        self.assertEqual(expected, actual)
+
+    def test_move_character_Left(self):
+        test_character = {"X-coordinate": 1, "Y-coordinate": 1}
+        move_character(character=test_character, direction="Left")
+        expected = (0, 1)
+        actual = (test_character["X-coordinate"], test_character["Y-coordinate"])
+        self.assertEqual(expected, actual)
