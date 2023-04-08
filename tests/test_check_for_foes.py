86866fbd4c245d820d25c3c40e56031801808404
Derek Woo <me@derekw.co>
2023-04-08 16:19:33 -0700

    Add unit tests to test_check_for_foes.py

---
+++ tests/test_check_for_foes.py
@@ -0,0 +1,31 @@
+"""
+Derek Woo
+A01351415
+"""
+
+
+from unittest import TestCase
+from unittest.mock import patch
+from combat_management.manage_foes import check_for_foes
+
+
+class TestCheckForFoes(TestCase):
+    @patch("random.randint", side_effect=[1])
+    def test_check_for_foes_when_foe_is_encountered_by_rolling_one(self, _):
+        actual = check_for_foes()
+        self.assertTrue(actual)
+
+    @patch("random.randint", side_effect=[2])
+    def test_check_for_foes_when_foe_is_not_encountered_by_rolling_two(self, _):
+        actual = check_for_foes()
+        self.assertFalse(actual)
+
+    @patch("random.randint", side_effect=[3])
+    def test_check_for_foes_when_foe_is_not_encountered_by_rolling_three(self, _):
+        actual = check_for_foes()
+        self.assertFalse(actual)
+
+    @patch("random.randint", side_effect=[4])
+    def test_check_for_foes_when_foe_is_not_encountered_by_rolling_four(self, _):
+        actual = check_for_foes()
+        self.assertFalse(actual)
