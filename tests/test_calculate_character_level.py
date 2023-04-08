b5100b138ea18a621c553955c03dc66b75ab880e
Derek Woo <me@derekw.co>
2023-04-08 14:41:00 -0700

    Add tests to test_calculate_character_level.py

--- tests/test_calculate_character_level.py
+++ tests/test_calculate_character_level.py
@@ -9,6 +9,26 @@ from character_management.character_level import calculate_character_level
 
 
 class TestCalculateCharacterLevel(TestCase):
+    def test_calculate_character_level_with_non_dictionary_character(self):
+        test_character = "String"
+        with self.assertRaises(TypeError):
+            calculate_character_level(character=test_character)
+
+    def test_calculate_character_level_without_current_hp_key(self):
+        test_character = {"Strength": 1}
+        with self.assertRaises(KeyError):
+            calculate_character_level(character=test_character)
+
+    def test_calculate_character_level_without_strength_key(self):
+        test_character = {"Current HP": 1}
+        with self.assertRaises(KeyError):
+            calculate_character_level(character=test_character)
+
+    def test_calculate_character_level_without_current_hp_or_strength_key(self):
+        test_character = {}
+        with self.assertRaises(KeyError):
+            calculate_character_level(character=test_character)
+
     def test_calculate_character_level_with_0_hp_and_strength(self):
         test_character = {"Current HP": 0, "Strength": 0}
         calculate_character_level(character=test_character)
