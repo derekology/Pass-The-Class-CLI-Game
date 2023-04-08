a42a7cdcdfca5892782fd4d76c18e196e2b26e31
Derek Woo <me@derekw.co>
2023-04-08 13:53:56 -0700

    Edit check_for_special_tile in character_level.py
    Add exception handling and update docstrings

--- character_management/character_level.py
+++ character_management/character_level.py
@@ -43,14 +43,23 @@ def check_for_special_tile(board: dict, character: dict, boss: bool = False) -> 
     :precondition: boss must be a boolean value
     :postcondition: verifies whether the current coordinate of the character contains a resource tile
     :return: whether the current coordinate of the character contains a resource tile as a boolean
+    :raises TypeError: if either board or character, or both, are not dictionaries
+    :raises TypeError: if boss is not a boolean value
+    :raises TypeError: if game board does not contain tuples of two positive integers as keys and strings as values
+    :raises KeyError: if character dictionary does not contain 'X-coordinate' or 'Y-coordinate' key, or both
+    :raises ValueError: if character's 'X-coordinate' or 'Y-coordinate' is not found on the game board
 
     >>> test_board = {(0, 0): "['R']", (1, 0): "[   ]"}
     >>> test_character = {"X-coordinate": 0, "Y-coordinate": 0}
-    >>> check_for_resources(board=test_board, character=test_character)
+    >>> check_for_special_tile(board=test_board, character=test_character, boss=False)
+    True
+    >>> test_board = {(0, 0): "['B']", (1, 0): "[   ]"}
+    >>> test_character = {"X-coordinate": 0, "Y-coordinate": 0}
+    >>> check_for_special_tile(board=test_board, character=test_character, boss=True)
     True
     >>> test_board = {(0, 0): "[   ]", (1, 0): "['R']"}
     >>> test_character = {"X-coordinate": 0, "Y-coordinate": 0}
-    >>> check_for_resources(board=test_board, character=test_character)
+    >>> check_for_special_tile(board=test_board, character=test_character, boss=False)
     False
     """
     if type(board) is not dict or type(character) is not dict:
