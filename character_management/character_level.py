330b6a5c135f2632d01ab4e4b9117c480598975e
Derek Woo <me@derekw.co>
2023-04-08 13:50:55 -0700

    Edit check_for_special_tile in character_level.py
    Add check for boss tile in function

--- character_management/character_level.py
+++ character_management/character_level.py
@@ -29,16 +29,18 @@ def calculate_character_level(character: dict) -> None:
     character["Level"] = character_level
 
 
-def check_for_resources(board: dict, character: dict) -> bool:
+def check_for_special_tile(board: dict, character: dict, boss: bool = False) -> bool:
     """
     Check if character's current location contains a resource.
 
     :param board: a dictionary representing a coordinate-based game board
     :param character: a dictionary representing the character's status
+    :param boss: a boolean value representing whether to search for a boss tile (default is False)
     :precondition: board must be a dictionary containing tuples of two positive integers as keys and strings as values
     :precondition: character must be a dictionary containing an "X-coordinate" key associated with an integer
     :precondition: character must be a dictionary containing a "Y-coordinate" key associated with an integer
     :precondition: values of "X-coordinate" and "Y-coordinate" keys in character must exist as coordinates in board
+    :precondition: boss must be a boolean value
     :postcondition: verifies whether the current coordinate of the character contains a resource tile
     :return: whether the current coordinate of the character contains a resource tile as a boolean
 
@@ -51,7 +53,19 @@ def check_for_resources(board: dict, character: dict) -> bool:
     >>> check_for_resources(board=test_board, character=test_character)
     False
     """
-    character_location = (character["X-coordinate"], character["Y-coordinate"])
+    if type(board) is not dict or type(character) is not dict:
+        raise TypeError("Character and game board must both be dictionaries.")
+
+    elif type(character["X-coordinate"]) is not int or type(character["Y-coordinate"]) is not int:
+        raise TypeError("Character's 'X-coordinate' and 'Y-coordinate' values must be integers")
+
+    elif character["X-coordinate"] not in [x for (x, _) in board] or \
+            character["Y-coordinate"] not in [y for (_, y) in board]:
+        raise ValueError("Character's coordinates must be on the game board.")
+
+    else:
+        search_target = "['B']" if boss else "['R']"
+        character_location = (character["X-coordinate"], character["Y-coordinate"])
 
     return board[character_location] == "['R']"
 
