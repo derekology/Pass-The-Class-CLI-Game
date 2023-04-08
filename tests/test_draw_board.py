dc74e4219d45ef3d41f96db55bb353c7612a5371
Derek Woo <me@derekw.co>
2023-04-08 14:41:37 -0700

    Add more unit tests to test_draw_board.py

--- tests/test_draw_board.py
+++ tests/test_draw_board.py
@@ -11,34 +11,6 @@ import io
 
 
 class TestDrawBoard(TestCase):
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_draw_board_with_smallest_board_possible(self, mock_print):
-        test_board = {(0, 0): "[   ]", (1, 0): "[   ]",
-                      (0, 1): "[   ]", (1, 1): "[   ]"}
-        draw_board(board=test_board, columns=2)
-        expected = "\n\n\n\n" \
-                   "[   ]\t[   ]\n" \
-                   "[   ]\t[   ]\n" \
-                   "\n"
-        actual = mock_print.getvalue()
-        self.assertEqual(expected, actual)
-
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_draw_board_with_large_board(self, mock_print):
-        test_board = {(0, 0): "[   ]", (1, 0): "[   ]", (2, 0): "[   ]", (3, 0): "[   ]",
-                      (0, 1): "[   ]", (1, 1): "[   ]", (2, 1): "[   ]", (3, 1): "[   ]",
-                      (0, 2): "[   ]", (1, 2): "[   ]", (2, 2): "[   ]", (3, 2): "[   ]",
-                      (0, 3): "[   ]", (1, 3): "[   ]", (2, 3): "[   ]", (3, 3): "[   ]"}
-        draw_board(board=test_board, columns=4)
-        expected = "\n\n\n\n" \
-                   "[   ]\t[   ]\t[   ]\t[   ]\n" \
-                   "[   ]\t[   ]\t[   ]\t[   ]\n" \
-                   "[   ]\t[   ]\t[   ]\t[   ]\n" \
-                   "[   ]\t[   ]\t[   ]\t[   ]\n" \
-                   "\n"
-        actual = mock_print.getvalue()
-        self.assertEqual(expected, actual)
-
     def test_draw_board_with_invalid_key_value(self):
         test_board = {(0, 0): 123, (1, 0): "[   ]",
                       (0, 1): "[   ]", (1, 1): "[   ]"}
@@ -63,6 +35,12 @@ class TestDrawBoard(TestCase):
         with self.assertRaises(TypeError):
             draw_board(board=test_board, columns=2)
 
+    def test_draw_board_with_non_integer_column(self):
+        test_board = {(0, 0): "[   ]", (1, 0): "[   ]",
+                      (0, 1): "[   ]", (1, 1): "[   ]"}
+        with self.assertRaises(TypeError):
+            draw_board(board=test_board, columns="a")
+
     def test_draw_board_with_one_column(self):
         test_board = {(0, 0): "[   ]", (1, 0): "[   ]",
                       (0, 1): "[   ]", (1, 1): "[   ]"}
@@ -75,3 +53,26 @@ class TestDrawBoard(TestCase):
         with self.assertRaises(ValueError):
             draw_board(board=test_board, columns=0)
 
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_draw_board_with_smallest_board_possible(self, mock_print):
+        test_board = {(0, 0): "[   ]", (1, 0): "[   ]",
+                      (0, 1): "[   ]", (1, 1): "[   ]"}
+        draw_board(board=test_board, columns=2)
+        expected = "[   ][   ]\n" \
+                   "[   ][   ]\n"
+        actual = mock_print.getvalue()
+        self.assertEqual(expected, actual)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_draw_board_with_large_board(self, mock_print):
+        test_board = {(0, 0): "[   ]", (1, 0): "[   ]", (2, 0): "[   ]", (3, 0): "[   ]",
+                      (0, 1): "[   ]", (1, 1): "[   ]", (2, 1): "[   ]", (3, 1): "[   ]",
+                      (0, 2): "[   ]", (1, 2): "[   ]", (2, 2): "[   ]", (3, 2): "[   ]",
+                      (0, 3): "[   ]", (1, 3): "[   ]", (2, 3): "[   ]", (3, 3): "[   ]"}
+        draw_board(board=test_board, columns=4)
+        expected = "[   ][   ][   ][   ]\n" \
+                   "[   ][   ][   ][   ]\n" \
+                   "[   ][   ][   ][   ]\n" \
+                   "[   ][   ][   ][   ]\n"
+        actual = mock_print.getvalue()
+        self.assertEqual(expected, actual)
