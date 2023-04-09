f739b4b869394f1adc9f4c21383a4dd70b84625e
Derek Woo <me@derekw.co>
2023-04-08 19:49:01 -0700

    Update variable names in test_create_foe.py
    Rename to align with updated function being tested

--- tests/test_create_foe.py
+++ tests/test_create_foe.py
@@ -15,33 +15,33 @@ class TestCreateFoe(TestCase):
         with self.assertRaises(TypeError):
             create_foe(character=test_character)
 
-    def test_create_foe_with_fighter_without_health_key(self):
-        test_character = {"Strength": 1}
+    def test_create_foe_with_fighter_without_reeses_key(self):
+        test_character = {"Smarts": 1}
         with self.assertRaises(KeyError):
             create_foe(character=test_character)
 
-    def test_create_foe_with_fighter_without_strength_key(self):
-        test_character = {"Current HP": 1}
+    def test_create_foe_with_fighter_without_smarts_key(self):
+        test_character = {"Reeses": 1}
         with self.assertRaises(KeyError):
             create_foe(character=test_character)
 
-    def test_create_foe_with_fighter_with_non_integer_health(self):
-        test_character = {"Current HP": "1", "Strength": 1}
+    def test_create_foe_with_fighter_with_non_integer_reeses(self):
+        test_character = {"Reeses": "1", "Smarts": 1}
         with self.assertRaises(ValueError):
             create_foe(character=test_character)
 
-    def test_create_foe_with_fighter_with_non_integer_strength(self):
-        test_character = {"Current HP": 1, "Strength": "1"}
+    def test_create_foe_with_fighter_with_non_integer_smarts(self):
+        test_character = {"Reeses": 1, "Smarts": "1"}
         with self.assertRaises(ValueError):
             create_foe(character=test_character)
 
-    def test_create_foe_with_fighter_with_negative_health(self):
-        test_character = {"Current HP": -1, "Strength": 1}
+    def test_create_foe_with_fighter_with_negative_reeses(self):
+        test_character = {"Reeses": -1, "Smarts": 1}
         with self.assertRaises(ValueError):
             create_foe(character=test_character)
 
-    def test_create_foe_with_fighter_with_negative_strength(self):
-        test_character = {"Current HP": 1, "Strength": -1}
+    def test_create_foe_with_fighter_with_negative_smarts(self):
+        test_character = {"Reeses": 1, "Smarts": -1}
         with self.assertRaises(ValueError):
             create_foe(character=test_character)
 
@@ -49,8 +49,8 @@ class TestCreateFoe(TestCase):
     @patch("random.random", side_effect=[0, 0])
     @patch("random.randint", side_effect=[1])
     def test_create_foe_with_minimum_random_values_and_non_boss(self, _, __, ___):
-        test_character = {"Current HP": 1, "Strength": 1}
-        expected = {"Name": "pop quiz", "Current HP": 1, "Strength": 1, "Level": 0, "Luck": 1}
+        test_character = {"Reeses": 1, "Smarts": 1}
+        expected = {"Name": "pop quiz", "Reeses": 1, "Smarts": 1, "Level": 0, "Luck": 1}
         actual = create_foe(character=test_character, boss=False)
         self.assertEqual(expected, actual)
 
@@ -58,8 +58,8 @@ class TestCreateFoe(TestCase):
     @patch("random.random", side_effect=[0.5, 0.5])
     @patch("random.randint", side_effect=[2])
     def test_create_foe_with_middle_random_values_and_non_boss(self, _, __, ___):
-        test_character = {"Current HP": 1, "Strength": 1}
-        expected = {"Name": "pop quiz", "Current HP": 2, "Strength": 2, "Level": 0, "Luck": 2}
+        test_character = {"Reeses": 1, "Smarts": 1}
+        expected = {"Name": "pop quiz", "Reeses": 1, "Smarts": 1, "Level": 0, "Luck": 2}
         actual = create_foe(character=test_character, boss=False)
         self.assertEqual(expected, actual)
 
@@ -67,14 +67,14 @@ class TestCreateFoe(TestCase):
     @patch("random.random", side_effect=[1, 1])
     @patch("random.randint", side_effect=[3])
     def test_create_foe_with_maximum_random_values_and_non_boss(self, _, __, ___):
-        test_character = {"Current HP": 1, "Strength": 1}
-        expected = {"Name": "pop quiz", "Current HP": 4, "Strength": 3, "Level": 1, "Luck": 3}
+        test_character = {"Reeses": 1, "Smarts": 1}
+        expected = {"Name": "pop quiz", "Reeses": 2, "Smarts": 2, "Level": 0, "Luck": 3}
         actual = create_foe(character=test_character, boss=False)
         self.assertEqual(expected, actual)
 
     @patch("random.randint", side_effect=[1])
     def test_create_foe_with_boss(self, _):
-        test_character = {"Current HP": 1, "Strength": 1}
-        expected = {"Name": "Final exam", "Current HP": 15, "Strength": 7, "Level": 4, "Luck": 1}
+        test_character = {"Reeses": 1, "Smarts": 1}
+        expected = {"Name": "Final exam", "Reeses": 15, "Smarts": 7, "Level": 4, "Luck": 1}
         actual = create_foe(character=test_character, boss=True)
         self.assertEqual(expected, actual)
