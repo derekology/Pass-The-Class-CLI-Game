9fd6364d062f36a67387833d85f4ad811c797852
Derek Woo <me@derekw.co>
2023-04-08 20:04:33 -0700

    Add unit tests to test_guessing_game.py

---
+++ tests/test_guessing_game.py
@@ -0,0 +1,23 @@
+"""
+Derek Woo
+A01351415
+"""
+
+
+from unittest import TestCase
+from unittest.mock import patch
+from utilities.guessing_game import guessing_game
+
+
+class TestGuessingGame(TestCase):
+    @patch("random.randint", side_effect=[2])
+    @patch("builtins.input", side_effect=["2"])
+    def test_guessing_game_after_a_correct_guess(self, _, __):
+        actual = guessing_game()
+        self.assertTrue(actual)
+
+    @patch("random.randint", side_effect=[2])
+    @patch("builtins.input", side_effect=["3"])
+    def test_guessing_game_after_an_incorrect_guess(self, _, __):
+        actual = guessing_game()
+        self.assertFalse(actual)
