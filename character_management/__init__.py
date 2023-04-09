5899d751e217106fe9c4317bc7137550e8981e1a
Derek Woo <me@derekw.co>
2023-04-08 19:38:09 -0700

    Update __init__.py in character_management
    Rename constants to align with theme

--- character_management/__init__.py
+++ character_management/__init__.py
@@ -1,7 +1,7 @@
-HP_UPGRADE_AMOUNT = 3
-STRENGTH_UPGRADE_AMOUNT = 1
+REESES_UPGRADE_AMOUNT = 3
+SMARTS_UPGRADE_AMOUNT = 1
 LUCK_UPGRADE_AMOUNT = 1
-MIN_HEALTH: int = 0
+MIN_REESES: int = 0
 MIN_CHARACTER_NAME_LENGTH = 0
 COORDINATE_CHANGE = {"Up": ("Y-coordinate", -1), "Right": ("X-coordinate", 1),
                      "Down": ("Y-coordinate", 1), "Left": ("X-coordinate", -1)}
