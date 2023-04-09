aa9e725ed1cc88a121f080c8dfca004e6c572ae5
Derek Woo <me@derekw.co>
2023-04-08 19:44:47 -0700

    Update variable names in manage_character.py
    Rename to align with game theme

--- character_management/manage_character.py
+++ character_management/manage_character.py
@@ -11,16 +11,16 @@ def make_character() -> dict:
 
     :postcondition: asks the user to input a name for their character
     :postcondition: creates a character at the origin and with the default skill attributes
-    :return: a level 1 character as a dictionary at coordinates (0, 0) with 5 health points, 1 Luck point,
-             and 1 Strength point
+    :return: a level 1 character as a dictionary at coordinates (0, 0) with 5 reeses points, 1 Luck point,
+             and 1 Smarts point
     """
     char_name = input("What should your character's name be?: ")
 
     while len(char_name) == MIN_CHARACTER_NAME_LENGTH:
         char_name = input("Name cannot be blank. Try again: ")
 
-    return {"Name": char_name, "X-coordinate": 0, "Y-coordinate": 0, "Current HP": 5,
-            "Luck": 1, "Strength": 1, "Level": 1}
+    return {"Name": char_name, "X-coordinate": 0, "Y-coordinate": 0, "Reeses": 5,
+            "Luck": 1, "Smarts": 1, "Level": 1}
 
 
 def is_alive(character: dict, alive_message=None, not_alive_message=None) -> bool:
@@ -30,21 +30,21 @@ def is_alive(character: dict, alive_message=None, not_alive_message=None) -> boo
     :param character: a dictionary representing the character's current status
     :param alive_message: optional string representing a message to be printed if character is still alive
     :param not_alive_message: optional string representing a message to be printed if character is not alive
-    :precondition: character must contain a "Current HP" key associated with an integer greater than or equal to zero
+    :precondition: character must contain a "Reeses" key associated with an integer greater than or equal to zero
     :precondition: alive_message must be a non-empty string
     :precondition: not_alive_message must be a non-empty string
-    :postcondition: checks whether the character has health points remaining
-    :return: whether the character's current number of health points is greater than zero as a boolean
+    :postcondition: checks whether the character has reeses points remaining
+    :return: whether the character's current number of reeses points is greater than zero as a boolean
     :raises ValueError: if alive_message is present but is an empty string or not a string
     :raises ValueError: if not_alive_message is present but is an empty string or not a string
 
-    >>> test_character = {"Current HP": 5}
+    >>> test_character = {"Reeses": 5}
     >>> is_alive(character=test_character)
     True
-    >>> test_character = {"Current HP": 0}
+    >>> test_character = {"Reeses": 0}
     >>> is_alive(character=test_character)
     False
-    >>> test_character = {"Current HP": -5}
+    >>> test_character = {"Reeses": -5}
     >>> is_alive(character=test_character)
     False
     """
@@ -55,7 +55,7 @@ def is_alive(character: dict, alive_message=None, not_alive_message=None) -> boo
         raise ValueError("not_alive_message must be a non-empty string.")
 
     else:
-        if character["Current HP"] > MIN_HEALTH:
+        if character["Reeses"] > MIN_REESES:
             print(alive_message) if alive_message else None
             return True
 
@@ -66,42 +66,42 @@ def is_alive(character: dict, alive_message=None, not_alive_message=None) -> boo
 
 def print_character_stats(character: dict) -> None:
     """
-    Print character name, current health, strength, and luck.
+    Print character name, current reeses, smarts, and luck.
 
     :param character: a dictionary representing the character's current status
     :precondition: character must contain a "Name" key associated with a non-empty string
     :precondition: character must contain a "Level" key associated with an integer greater than or equal to zero
-    :precondition: character must contain a "Current HP" key associated with an integer greater than or equal to zero
-    :precondition: character must contain a "Strength" key associated with an integer greater than or equal to zero
+    :precondition: character must contain a "Reeses" key associated with an integer greater than or equal to zero
+    :precondition: character must contain a "Smarts" key associated with an integer greater than or equal to zero
     :precondition: character must contain a "Luck" key associated with an integer greater than or equal to zero
-    :postcondition: prints character's name, health points, strength, and luck attributes
+    :postcondition: prints character's name, reeses points, smarts, and luck attributes
     :raises KeyError: if character dictionary does not contain a "Name" key
     :raises KeyError: if character dictionary does not contain a "Level" key
-    :raises KeyError: if character dictionary does not contain a "Current HP" key
-    :raises KeyError: if character dictionary does not contain a "Strength" key
+    :raises KeyError: if character dictionary does not contain a "Reeses" key
+    :raises KeyError: if character dictionary does not contain a "Smarts" key
     :raises KeyError: if character dictionary does not contain a "Luck" key
     :raises TypeError: if character dictionary's "Name" value is not a non-empty string
     :raises ValueError: if character dictionary's "Level" value is not positive nonzero integer
-    :raises ValueError: if character dictionary's "Current HP" value is not positive nonzero integer
-    :raises ValueError: if character dictionary's "Strength" value is not positive nonzero integer
+    :raises ValueError: if character dictionary's "Reeses" value is not positive nonzero integer
+    :raises ValueError: if character dictionary's "Smarts" value is not positive nonzero integer
     :raises ValueError: if character dictionary's "Luck" value is not positive nonzero integer
     """
     if type(character) is not dict:
         raise TypeError("Character must be a dictionary.")
 
-    elif [key for key in ["Name", "Level", "Current HP", "Strength", "Luck"] if key not in character.keys()]:
-        raise KeyError("Character dictionary must contain 'Current HP', 'Strength', and 'Luck' keys")
+    elif [key for key in ["Name", "Level", "Reeses", "Smarts", "Luck"] if key not in character.keys()]:
+        raise KeyError("Character dictionary must contain 'Reeses', 'Smarts', and 'Luck' keys")
 
     elif type(character["Name"]) is not str or len(character["Name"]) == 0:
         raise TypeError("Character name must be a non-empty string.")
 
-    elif [value for key, value in character.items() if key in ["Level", "Current HP", "Strength", "Luck"]
+    elif [value for key, value in character.items() if key in ["Level", "Reeses", "Smarts", "Luck"]
             and (type(value) is not int or value < 0)]:
-        raise ValueError("Character Level, Health, Strength, and Luck must be integers greater than or equal to zero")
+        raise ValueError("Character Level, Reeses, Smarts, and Luck must be integers greater than or equal to zero")
 
     else:
-        print(f"{character['Name']} (Level {character['Level']})\nHealth: {character['Current HP']}\n"
-              f"Strength: {character['Strength']}\nLuck:{character['Luck']}")
+        print(f"{character['Name']} (Level {character['Level']})\nReeses: {character['Reeses']}\n"
+              f"Smarts: {character['Smarts']}\nLuck:{character['Luck']}")
 
 
 def main():
