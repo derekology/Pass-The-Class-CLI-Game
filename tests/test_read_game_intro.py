ee9ab4bfa378fabb88cf49fa70037d64cf6ef241
Derek Woo <me@derekw.co>
2023-04-09 00:12:59 -0700

    Add unit tests to test_read_game_intro.py

---
+++ tests/test_read_game_intro.py
@@ -0,0 +1,44 @@
+"""
+Derek Woo
+A01351415
+"""
+
+
+from unittest import TestCase
+from unittest.mock import patch
+import io
+from utilities.read_game_intro import read_game_intro
+
+
+class TestReadGameIntro(TestCase):
+    def test_read_game_intro_with_nonexistent_file(self):
+        with self.assertRaises(FileNotFoundError):
+            read_game_intro("nonexistent_file")
+
+    @patch("builtins.open", side_effect=[io.StringIO('')])
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_read_game_intro_with_an_empty_file(self, mock_output, _):
+        read_game_intro("test_read_game_intro.py")
+        actual = mock_output.getvalue()
+        expected = ""
+        self.assertEqual(expected, actual)
+
+    @patch("builtins.open", side_effect=[io.StringIO('This is one line in my file.')])
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_read_game_intro_with_a_one_line_file(self, mock_output, _):
+        read_game_intro("test_read_game_intro.py")
+        actual = mock_output.getvalue()
+        expected = "\nThis is one line in my file."
+        self.assertEqual(expected, actual)
+
+    @patch("builtins.open", side_effect=[io.StringIO('This is one line in my file.\n'
+                                                     'This is a second line in my file.\n'
+                                                     'This is a third line in my file.')])
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_read_game_intro_with_a_file_with_more_than_one_line(self, mock_output, _):
+        read_game_intro("test_read_game_intro.py")
+        actual = mock_output.getvalue()
+        expected = "\nThis is one line in my file.\n" \
+                   "\nThis is a second line in my file.\n" \
+                   "\nThis is a third line in my file."
+        self.assertEqual(expected, actual)
