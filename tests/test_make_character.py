eb64b74e0fe44994bdd8a43b0822003a0151a901
Derek Woo <me@derekw.co>
2023-04-08 19:52:55 -0700

    Update variable names in test_make_character.py
    Rename variables to align with updated function being tested

--- tests/test_make_character.py
+++ tests/test_make_character.py
@@ -12,35 +12,35 @@ from character_management.manage_character import make_character
 class TestMakeCharacter(TestCase):
     @patch("builtins.input", side_effect=["Name"])
     def test_make_character_with_a_one_word_name(self, _):
-        expected = {"Name": "Name", "X-coordinate": 0, "Y-coordinate": 0, "Current HP": 5,
-                    "Luck": 1, "Strength": 1, "Level": 1}
+        expected = {"Name": "Name", "X-coordinate": 0, "Y-coordinate": 0, "Reeses": 5,
+                    "Luck": 1, "Smarts": 1, "Level": 1}
         actual = make_character()
         self.assertEqual(expected, actual)
 
     @patch("builtins.input", side_effect=["Test Name"])
     def test_make_character_with_a_name_containing_space(self, _):
-        expected = {"Name": "Test Name", "X-coordinate": 0, "Y-coordinate": 0, "Current HP": 5,
-                    "Luck": 1, "Strength": 1, "Level": 1}
+        expected = {"Name": "Test Name", "X-coordinate": 0, "Y-coordinate": 0, "Reeses": 5,
+                    "Luck": 1, "Smarts": 1, "Level": 1}
         actual = make_character()
         self.assertEqual(expected, actual)
 
     @patch("builtins.input", side_effect=["1314"])
     def test_make_character_with_a_name_containing_number(self, _):
-        expected = {"Name": "1314", "X-coordinate": 0, "Y-coordinate": 0, "Current HP": 5,
-                    "Luck": 1, "Strength": 1, "Level": 1}
+        expected = {"Name": "1314", "X-coordinate": 0, "Y-coordinate": 0, "Reeses": 5,
+                    "Luck": 1, "Smarts": 1, "Level": 1}
         actual = make_character()
         self.assertEqual(expected, actual)
 
     @patch("builtins.input", side_effect=["?Name!"])
     def test_make_character_with_a_name_containing_punctuation(self, _):
-        expected = {"Name": "?Name!", "X-coordinate": 0, "Y-coordinate": 0, "Current HP": 5,
-                    "Luck": 1, "Strength": 1, "Level": 1}
+        expected = {"Name": "?Name!", "X-coordinate": 0, "Y-coordinate": 0, "Reeses": 5,
+                    "Luck": 1, "Smarts": 1, "Level": 1}
         actual = make_character()
         self.assertEqual(expected, actual)
 
     @patch("builtins.input", side_effect=["HaX0R321!"])
     def test_make_character_with_a_mixed_character_name(self, _):
-        expected = {"Name": "HaX0R321!", "X-coordinate": 0, "Y-coordinate": 0, "Current HP": 5,
-                    "Luck": 1, "Strength": 1, "Level": 1}
+        expected = {"Name": "HaX0R321!", "X-coordinate": 0, "Y-coordinate": 0, "Reeses": 5,
+                    "Luck": 1, "Smarts": 1, "Level": 1}
         actual = make_character()
         self.assertEqual(expected, actual)
