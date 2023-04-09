76f1e1336ae8c8d958d2069887b632d2914e066b
Derek Woo <me@derekw.co>
2023-04-08 19:51:52 -0700

    Update variables in test_get_upgrade_choice.py
    Rename variables to align with updated function being tested

--- tests/test_get_upgrade_choice.py
+++ tests/test_get_upgrade_choice.py
@@ -11,62 +11,62 @@ from character_management.character_level import get_upgrade_choice
 
 class TestGetUpgradeChoice(TestCase):
 
-    @patch("builtins.input", side_effect=["Health"])
-    def test_get_upgrade_choice_with_health_upgrade_with_full_word(self, _):
-        expected = "Health"
+    @patch("builtins.input", side_effect=["Reeses"])
+    def test_get_upgrade_choice_with_reeses_upgrade_with_full_word(self, _):
+        expected = "Reeses"
         actual = get_upgrade_choice()
         self.assertEqual(expected, actual)
 
-    @patch("builtins.input", side_effect=["HEALTH"])
-    def test_get_upgrade_choice_health_upgrade_with_uppercase_full_word(self, _):
-        expected = "Health"
+    @patch("builtins.input", side_effect=["REESES"])
+    def test_get_upgrade_choice_reeses_upgrade_with_uppercase_full_word(self, _):
+        expected = "Reeses"
         actual = get_upgrade_choice()
         self.assertEqual(expected, actual)
 
-    @patch("builtins.input", side_effect=["health"])
-    def test_get_upgrade_choice_with_health_upgrade_with_lowercase_full_word(self, _):
-        expected = "Health"
+    @patch("builtins.input", side_effect=["reeses"])
+    def test_get_upgrade_choice_with_reeses_upgrade_with_lowercase_full_word(self, _):
+        expected = "Reeses"
         actual = get_upgrade_choice()
         self.assertEqual(expected, actual)
 
-    @patch("builtins.input", side_effect=["H"])
-    def test_get_upgrade_choice_with_health_upgrade_with_uppercase_shorthand(self, _):
-        expected = "H"
+    @patch("builtins.input", side_effect=["R"])
+    def test_get_upgrade_choice_with_reeses_upgrade_with_uppercase_shorthand(self, _):
+        expected = "R"
         actual = get_upgrade_choice()
         self.assertEqual(expected, actual)
 
-    @patch("builtins.input", side_effect=["h"])
-    def test_get_upgrade_choice_with_health_upgrade_with_lowercase_shorthand(self, _):
-        expected = "H"
+    @patch("builtins.input", side_effect=["r"])
+    def test_get_upgrade_choice_with_reeses_upgrade_with_lowercase_shorthand(self, _):
+        expected = "R"
         actual = get_upgrade_choice()
         self.assertEqual(expected, actual)
 
-    @patch("builtins.input", side_effect=["Strength"])
-    def test_get_upgrade_choice_with_strength_upgrade_with_full_word(self, _):
-        expected = "Strength"
+    @patch("builtins.input", side_effect=["Smarts"])
+    def test_get_upgrade_choice_with_smarts_upgrade_with_full_word(self, _):
+        expected = "Smarts"
         actual = get_upgrade_choice()
         self.assertEqual(expected, actual)
 
-    @patch("builtins.input", side_effect=["STRENGTH"])
-    def test_get_upgrade_choice_with_strength_upgrade_with_uppercase_full_word(self, _):
-        expected = "Strength"
+    @patch("builtins.input", side_effect=["SMARTS"])
+    def test_get_upgrade_choice_with_smarts_upgrade_with_uppercase_full_word(self, _):
+        expected = "Smarts"
         actual = get_upgrade_choice()
         self.assertEqual(expected, actual)
 
-    @patch("builtins.input", side_effect=["strength"])
-    def test_get_upgrade_choice_with_strength_upgrade_with_lowercase_full_word(self, _):
-        expected = "Strength"
+    @patch("builtins.input", side_effect=["smarts"])
+    def test_get_upgrade_choice_with_smarts_upgrade_with_lowercase_full_word(self, _):
+        expected = "Smarts"
         actual = get_upgrade_choice()
         self.assertEqual(expected, actual)
 
     @patch("builtins.input", side_effect=["S"])
-    def test_get_upgrade_choice_with_strength_upgrade_with_uppercase_shorthand(self, _):
+    def test_get_upgrade_choice_with_smarts_upgrade_with_uppercase_shorthand(self, _):
         expected = "S"
         actual = get_upgrade_choice()
         self.assertEqual(expected, actual)
 
     @patch("builtins.input", side_effect=["s"])
-    def test_get_upgrade_choice_with_strength_upgrade_with_lowercase_shorthand(self, _):
+    def test_get_upgrade_choice_with_smarts_upgrade_with_lowercase_shorthand(self, _):
         expected = "S"
         actual = get_upgrade_choice()
         self.assertEqual(expected, actual)
