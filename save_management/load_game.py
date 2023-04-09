909e393ce26cb483ec4205a11d579a89ad715e15
Derek Woo <me@derekw.co>
2023-04-09 00:19:49 -0700

    Add exception raising to load_game in load_game.py

--- save_management/load_game.py
+++ save_management/load_game.py
@@ -10,21 +10,30 @@ def load_game() -> tuple:
     :postcondition: prompts use for another name if save data not found
     :postcondition: opens saved character file and parses data into game variables
     :return: the character and board save state as a tuple
+    :raises ValueError: if file represented by filename cannot be loaded as a JSON-formatted string
     """
-    character_name = input("What is your character name?: ")
+    character_name = None
 
-    while not pathlib.Path(f"savegames/{character_name}.save").is_file():
-        character_name = input("Save not found. What is your saved character's name?: ")
+    while not character_name:
+        character_name = input("What is your saved character's name?: ")
 
-    with open(f"savegames/{character_name}.save", "r") as save_file:
-        data = save_file.read()
+        while not pathlib.Path(f"savegames/{character_name}.save").is_file():
+            character_name = input("Save not found. What is your saved character's name?: ")
 
-    loaded_data = json.loads(data)
+        with open(f"savegames/{character_name}.save", "r") as save_file:
+            data = save_file.read()
 
-    save_state = (loaded_data["character"], loaded_data["board"]["rows"],
-                  loaded_data["board"]["columns"], loaded_data["board"]["resource"])
+        try:
+            loaded_data = json.loads(data)
 
-    return save_state
+        except ValueError as error_message:
+            print(error_message)
+
+        else:
+            save_state = (loaded_data["character"], loaded_data["board"]["rows"], loaded_data["board"]["columns"],
+                          loaded_data["board"]["resource"], loaded_data["board"]["boss"])
+
+            return save_state
 
 
 def main():
