21faaefa56fff971f5f28c3715ff830fb20d3a6a
Derek Woo <me@derekw.co>
2023-04-08 19:52:31 -0700

    Update variable names in test_is_alive.py
    Rename variables to align with updated function being tested

--- tests/test_is_alive.py
+++ tests/test_is_alive.py
@@ -11,29 +11,29 @@ import io
 
 
 class TestIsAlive(TestCase):
-    def test_is_alive_with_character_that_has_plenty_of_hp(self):
-        test_character = {"Current HP": 5}
+    def test_is_alive_with_character_that_has_plenty_of_reeses(self):
+        test_character = {"Reeses": 5}
         actual = is_alive(character=test_character)
         self.assertTrue(actual)
 
-    def test_is_alive_with_character_that_has_1_hp(self):
-        test_character = {"Current HP": 1}
+    def test_is_alive_with_character_that_has_1_reeses(self):
+        test_character = {"Reeses": 1}
         actual = is_alive(character=test_character)
         self.assertTrue(actual)
 
-    def test_is_alive_with_character_that_is_not_alive_with_0HP(self):
-        test_character = {"Current HP": 0}
+    def test_is_alive_with_character_that_is_not_alive_with_0_reeses(self):
+        test_character = {"Reeses": 0}
         actual = is_alive(character=test_character)
         self.assertFalse(actual)
 
-    def test_is_alive_with_character_that_is_not_alive_with_negative_HP(self):
-        test_character = {"Current HP": -5}
+    def test_is_alive_with_character_that_is_not_alive_with_negative_reeses(self):
+        test_character = {"Reeses": -5}
         actual = is_alive(character=test_character)
         self.assertFalse(actual)
 
     @patch("sys.stdout", new_callable=io.StringIO)
     def test_is_alive_with_alive_message(self, mock_output):
-        test_character = {"Current HP": 5}
+        test_character = {"Reeses": 5}
         is_alive(character=test_character, alive_message="Test Alive Message")
         expected = "Test Alive Message\n"
         actual = mock_output.getvalue()
@@ -41,7 +41,7 @@ class TestIsAlive(TestCase):
 
     @patch("sys.stdout", new_callable=io.StringIO)
     def test_is_alive_with_not_alive_message(self, mock_output):
-        test_character = {"Current HP": 0}
+        test_character = {"Reeses": 0}
         is_alive(character=test_character, not_alive_message="Test Not Alive Message")
         expected = "Test Not Alive Message\n"
         actual = mock_output.getvalue()
