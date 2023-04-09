f44fae0d7db7bb4b72f11bd4f696f921e78689b2
Derek Woo <me@derekw.co>
2023-04-08 19:47:17 -0700

    Update variable names in test_apply_resource.py
    Rename to align with updated function being tested

--- tests/test_apply_resource.py
+++ tests/test_apply_resource.py
@@ -16,169 +16,169 @@ class TestApplyResource(TestCase):
         with self.assertRaises(TypeError):
             apply_resource(character=test_character)
     
-    def test_apply_resource_without_current_hp_key(self):
-        test_character = {"Strength": 1, "Luck": 1}
+    def test_apply_resource_without_reeses_key(self):
+        test_character = {"Smarts": 1, "Luck": 1}
         with self.assertRaises(KeyError):
             apply_resource(character=test_character)
     
-    def test_apply_resource_without_strength_key(self):
-        test_character = {"Current HP": 1, "Luck": 1}
+    def test_apply_resource_without_smarts_key(self):
+        test_character = {"Reeses": 1, "Luck": 1}
         with self.assertRaises(KeyError):
             apply_resource(character=test_character)
     
     def test_apply_resource_without_luck_key(self):
-        test_character = {"Current HP": 1, "Strength": 1}
+        test_character = {"Reeses": 1, "Smarts": 1}
         with self.assertRaises(KeyError):
             apply_resource(character=test_character)
     
-    def test_apply_resource_with_non_integer_health(self):
-        test_character = {"Current HP": "1", "Strength": 1, "Luck": 1}
+    def test_apply_resource_with_non_integer_reeses(self):
+        test_character = {"Reeses": "1", "Smarts": 1, "Luck": 1}
         with self.assertRaises(ValueError):
             apply_resource(character=test_character)
     
-    def test_apply_resource_with_non_integer_strength(self):
-        test_character = {"Current HP": 1, "Strength": "1", "Luck": 1}
+    def test_apply_resource_with_non_integer_smarts(self):
+        test_character = {"Reeses": 1, "Smarts": "1", "Luck": 1}
         with self.assertRaises(ValueError):
             apply_resource(character=test_character)
     
     def test_apply_resource_with_non_integer_luck(self):
-        test_character = {"Current HP": 1, "Strength": 1, "Luck": "1"}
+        test_character = {"Reeses": 1, "Smarts": 1, "Luck": "1"}
         with self.assertRaises(ValueError):
             apply_resource(character=test_character)
     
-    def test_apply_resource_with_negative_health(self):
-        test_character = {"Current HP": -1, "Strength": 1, "Luck": 1}
+    def test_apply_resource_with_negative_reeses(self):
+        test_character = {"Reeses": -1, "Smarts": 1, "Luck": 1}
         with self.assertRaises(ValueError):
             apply_resource(character=test_character)
     
-    def test_apply_resource_with_negative_strength(self):
-        test_character = {"Current HP": 1, "Strength": -1, "Luck": 1}
+    def test_apply_resource_with_negative_smarts(self):
+        test_character = {"Reeses": 1, "Smarts": -1, "Luck": 1}
         with self.assertRaises(ValueError):
             apply_resource(character=test_character)
     
     def test_apply_resource_with_negative_luck(self):
-        test_character = {"Current HP": 1, "Strength": 1, "Luck": -1}
+        test_character = {"Reeses": 1, "Smarts": 1, "Luck": -1}
         with self.assertRaises(ValueError):
             apply_resource(character=test_character)
     
-    @patch("builtins.input", side_effect=["Health"])
-    def test_apply_resource_with_health_upgrade_with_full_word(self, _):
-        test_character = {"Current HP": 0, "Strength": 0, "Luck": 0}
+    @patch("builtins.input", side_effect=["Reeses"])
+    def test_apply_resource_with_reeses_upgrade_with_full_word(self, _):
+        test_character = {"Reeses": 0, "Smarts": 0, "Luck": 0}
         apply_resource(character=test_character)
-        expected = {"Current HP": 3, "Strength": 0, "Luck": 0}
+        expected = {"Reeses": 3, "Smarts": 0, "Luck": 0}
+        actual = test_character
+        self.assertEqual(expected, actual)
+
+    @patch("builtins.input", side_effect=["REESES"])
+    def test_apply_resource_with_reeses_upgrade_with_uppercase_full_word(self, _):
+        test_character = {"Reeses": 0, "Smarts": 0, "Luck": 0}
+        apply_resource(character=test_character)
+        expected = {"Reeses": 3, "Smarts": 0, "Luck": 0}
         actual = test_character
         self.assertEqual(expected, actual)
     
-    @patch("builtins.input", side_effect=["HEALTH"])
-    def test_apply_resource_with_health_upgrade_with_uppercase_full_word(self, _):
-        test_character = {"Current HP": 0, "Strength": 0, "Luck": 0}
+    @patch("builtins.input", side_effect=["reeses"])
+    def test_apply_resource_with_reeses_upgrade_with_lowercase_full_word(self, _):
+        test_character = {"Reeses": 0, "Smarts": 0, "Luck": 0}
         apply_resource(character=test_character)
-        expected = {"Current HP": 3, "Strength": 0, "Luck": 0}
+        expected = {"Reeses": 3, "Smarts": 0, "Luck": 0}
         actual = test_character
         self.assertEqual(expected, actual)
     
-    @patch("builtins.input", side_effect=["health"])
-    def test_apply_resource_with_health_upgrade_with_lowercase_full_word(self, _):
-        test_character = {"Current HP": 0, "Strength": 0, "Luck": 0}
+    @patch("builtins.input", side_effect=["R"])
+    def test_apply_resource_with_reeses_upgrade_with_uppercase_shorthand(self, _):
+        test_character = {"Reeses": 0, "Smarts": 0, "Luck": 0}
         apply_resource(character=test_character)
-        expected = {"Current HP": 3, "Strength": 0, "Luck": 0}
+        expected = {"Reeses": 3, "Smarts": 0, "Luck": 0}
         actual = test_character
         self.assertEqual(expected, actual)
     
-    @patch("builtins.input", side_effect=["H"])
-    def test_apply_resource_with_health_upgrade_with_uppercase_shorthand(self, _):
-        test_character = {"Current HP": 0, "Strength": 0, "Luck": 0}
+    @patch("builtins.input", side_effect=["r"])
+    def test_apply_resource_with_reeses_upgrade_with_lowercase_shorthand(self, _):
+        test_character = {"Reeses": 0, "Smarts": 0, "Luck": 0}
         apply_resource(character=test_character)
-        expected = {"Current HP": 3, "Strength": 0, "Luck": 0}
+        expected = {"Reeses": 3, "Smarts": 0, "Luck": 0}
         actual = test_character
         self.assertEqual(expected, actual)
     
-    @patch("builtins.input", side_effect=["h"])
-    def test_apply_resource_with_health_upgrade_with_lowercase_shorthand(self, _):
-        test_character = {"Current HP": 0, "Strength": 0, "Luck": 0}
+    @patch("builtins.input", side_effect=["Smarts"])
+    def test_apply_resource_with_smarts_upgrade_with_full_word(self, _):
+        test_character = {"Reeses": 0, "Smarts": 0, "Luck": 0}
         apply_resource(character=test_character)
-        expected = {"Current HP": 3, "Strength": 0, "Luck": 0}
+        expected = {"Reeses": 0, "Smarts": 1, "Luck": 0}
         actual = test_character
         self.assertEqual(expected, actual)
     
-    @patch("builtins.input", side_effect=["Strength"])
-    def test_apply_resource_with_strength_upgrade_with_full_word(self, _):
-        test_character = {"Current HP": 0, "Strength": 0, "Luck": 0}
+    @patch("builtins.input", side_effect=["SMARTS"])
+    def test_apply_resource_with_smarts_upgrade_with_uppercase_full_word(self, _):
+        test_character = {"Reeses": 0, "Smarts": 0, "Luck": 0}
         apply_resource(character=test_character)
-        expected = {"Current HP": 0, "Strength": 1, "Luck": 0}
+        expected = {"Reeses": 0, "Smarts": 1, "Luck": 0}
         actual = test_character
         self.assertEqual(expected, actual)
     
-    @patch("builtins.input", side_effect=["STRENGTH"])
-    def test_apply_resource_with_strength_upgrade_with_uppercase_full_word(self, _):
-        test_character = {"Current HP": 0, "Strength": 0, "Luck": 0}
+    @patch("builtins.input", side_effect=["smarts"])
+    def test_apply_resource_with_smarts_upgrade_with_lowercase_full_word(self, _):
+        test_character = {"Reeses": 0, "Smarts": 0, "Luck": 0}
         apply_resource(character=test_character)
-        expected = {"Current HP": 0, "Strength": 1, "Luck": 0}
-        actual = test_character
-        self.assertEqual(expected, actual)
-    
-    @patch("builtins.input", side_effect=["strength"])
-    def test_apply_resource_with_strength_upgrade_with_lowercase_full_word(self, _):
-        test_character = {"Current HP": 0, "Strength": 0, "Luck": 0}
-        apply_resource(character=test_character)
-        expected = {"Current HP": 0, "Strength": 1, "Luck": 0}
+        expected = {"Reeses": 0, "Smarts": 1, "Luck": 0}
         actual = test_character
         self.assertEqual(expected, actual)
     
     @patch("builtins.input", side_effect=["S"])
-    def test_apply_resource_with_strength_upgrade_with_uppercase_shorthand(self, _):
-        test_character = {"Current HP": 0, "Strength": 0, "Luck": 0}
+    def test_apply_resource_with_smarts_upgrade_with_uppercase_shorthand(self, _):
+        test_character = {"Reeses": 0, "Smarts": 0, "Luck": 0}
         apply_resource(character=test_character)
-        expected = {"Current HP": 0, "Strength": 1, "Luck": 0}
+        expected = {"Reeses": 0, "Smarts": 1, "Luck": 0}
         actual = test_character
         self.assertEqual(expected, actual)
     
     @patch("builtins.input", side_effect=["s"])
-    def test_apply_resource_with_strength_upgrade_with_lowercase_shorthand(self, _):
-        test_character = {"Current HP": 0, "Strength": 0, "Luck": 0}
+    def test_apply_resource_with_smarts_upgrade_with_lowercase_shorthand(self, _):
+        test_character = {"Reeses": 0, "Smarts": 0, "Luck": 0}
         apply_resource(character=test_character)
-        expected = {"Current HP": 0, "Strength": 1, "Luck": 0}
+        expected = {"Reeses": 0, "Smarts": 1, "Luck": 0}
         actual = test_character
         self.assertEqual(expected, actual)
     
     @patch("builtins.input", side_effect=["Luck"])
     def test_apply_resource_with_luck_upgrade_with_full_word(self, _):
-        test_character = {"Current HP": 0, "Strength": 0, "Luck": 0}
+        test_character = {"Reeses": 0, "Smarts": 0, "Luck": 0}
         apply_resource(character=test_character)
-        expected = {"Current HP": 0, "Strength": 0, "Luck": 1}
+        expected = {"Reeses": 0, "Smarts": 0, "Luck": 1}
         actual = test_character
         self.assertEqual(expected, actual)
     
     @patch("builtins.input", side_effect=["LUCK"])
     def test_apply_resource_with_luck_upgrade_with_uppercase_full_word(self, _):
-        test_character = {"Current HP": 0, "Strength": 0, "Luck": 0}
+        test_character = {"Reeses": 0, "Smarts": 0, "Luck": 0}
         apply_resource(character=test_character)
-        expected = {"Current HP": 0, "Strength": 0, "Luck": 1}
+        expected = {"Reeses": 0, "Smarts": 0, "Luck": 1}
         actual = test_character
         self.assertEqual(expected, actual)
     
     @patch("builtins.input", side_effect=["luck"])
     def test_apply_resource_with_luck_upgrade_with_lowercase_full_word(self, _):
-        test_character = {"Current HP": 0, "Strength": 0, "Luck": 0}
+        test_character = {"Reeses": 0, "Smarts": 0, "Luck": 0}
         apply_resource(character=test_character)
-        expected = {"Current HP": 0, "Strength": 0, "Luck": 1}
+        expected = {"Reeses": 0, "Smarts": 0, "Luck": 1}
         actual = test_character
         self.assertEqual(expected, actual)
     
     @patch("builtins.input", side_effect=["L"])
     def test_apply_resource_with_luck_upgrade_with_uppercase_shorthand(self, _):
-        test_character = {"Current HP": 0, "Strength": 0, "Luck": 0}
+        test_character = {"Reeses": 0, "Smarts": 0, "Luck": 0}
         apply_resource(character=test_character)
-        expected = {"Current HP": 0, "Strength": 0, "Luck": 1}
+        expected = {"Reeses": 0, "Smarts": 0, "Luck": 1}
         actual = test_character
         self.assertEqual(expected, actual)
 
     @patch('builtins.input', side_effect=["l"])
     def test_apply_resource_with_luck_upgrade_with_lowercase_shorthand(self, _):
         get_upgrade_choice.return_value = 'l'
-        test_character = {"Current HP": 0, "Strength": 0, "Luck": 0}
+        test_character = {"Reeses": 0, "Smarts": 0, "Luck": 0}
         apply_resource(character=test_character)
-        expected = {"Current HP": 0, "Strength": 0, "Luck": 1}
+        expected = {"Reeses": 0, "Smarts": 0, "Luck": 1}
         actual = test_character
         self.assertEqual(expected, actual)
 
