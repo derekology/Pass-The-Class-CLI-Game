bff099faa5f297fbd5fa6a2a0bb0986fc8332cea
Derek Woo <me@derekw.co>
2023-04-09 20:35:11 -0700

    Edit upgrade values constant in __init__.py

--- character_management/__init__.py
+++ character_management/__init__.py
@@ -1,6 +1,4 @@
-REESES_UPGRADE_AMOUNT = 3
-SMARTS_UPGRADE_AMOUNT = 1
-LUCK_UPGRADE_AMOUNT = 1
+UPGRADE_AMOUNTS = {"Reeses": 3, "Smarts": 1, "Luck": 1}
 MIN_REESES: int = 0
 MIN_CHARACTER_NAME_LENGTH = 0
 COORDINATE_CHANGE = {"Up": ("Y-coordinate", -1), "Right": ("X-coordinate", 1),
