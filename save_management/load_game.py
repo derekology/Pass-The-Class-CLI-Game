202b836533d730c5875e6a45019e50a0bb77f62a
Derek Woo <me@derekw.co>
2023-04-09 20:53:50 -0700

    Add load_game exception handler in load_game.py
    Add error handling for Key Errors raised when corrupted save file is loaded

--- save_management/load_game.py
+++ save_management/load_game.py
@@ -16,30 +16,29 @@ def load_game() -> tuple:
     :postcondition: prompts use for another name if save data not found
     :postcondition: opens saved character file and parses data into game variables
     :return: the character and board save state as a tuple
-    :raises ValueError: if file represented by filename cannot be loaded as a JSON-formatted string
     """
-    character_name = None
+    character_name = input(f"What is your saved character's name?: ")
 
-    while not character_name:
-        character_name = input(f"What is your saved character's name?: ")
+    while not pathlib.Path(f"savegames/{character_name}.save").is_file():
+        character_name = input(f"Save not found. What is your saved character's name?: ")
 
-        while not pathlib.Path(f"savegames/{character_name}.save").is_file():
-            character_name = input(f"Save not found. What is your saved character's name?: ")
+    with open(f"savegames/{character_name}.save", "r") as save_file:
+        data = save_file.read()
 
-        with open(f"savegames/{character_name}.save", "r") as save_file:
-            data = save_file.read()
+    try:
+        loaded_data = json.loads(data)
 
-        try:
-            loaded_data = json.loads(data)
+        save_state = (loaded_data["character"], loaded_data["board"]["rows"], loaded_data["board"]["columns"],
+                      loaded_data["board"]["resource"], loaded_data["board"]["boss"], loaded_data["week"])
 
-        except ValueError as error_message:
-            print(error_message)
+    except ValueError:
+        print("Unable to load save data. Please ensure that your file is a Pass the Class save game.")
 
-        else:
-            save_state = (loaded_data["character"], loaded_data["board"]["rows"], loaded_data["board"]["columns"],
-                          loaded_data["board"]["resource"], loaded_data["board"]["boss"], loaded_data["week"])
+    except KeyError:
+        print("Unable to load save data. Please ensure the file is not corrupted.")
 
-            return save_state
+    else:
+        return save_state
 
 
 def main():
